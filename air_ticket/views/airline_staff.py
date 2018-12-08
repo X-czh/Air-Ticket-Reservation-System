dbe374207fa0bdde58806c77d9fae4c13c3fa8c9
X-czh <m1a2@outlook.com>
2018-12-08 22:14:44 +0800

    update airline_staff

--- air_ticket/views/airline_staff.py
+++ air_ticket/views/airline_staff.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for
 from pymysql import MySQLError
+from datetime import date, datetime, timedelta
+from dateutil.relativedelta import relativedelta
 from air_ticket import conn
 from air_ticket.utils import requires_login_airline_staff
 
@@ -47,17 +49,23 @@ def createNewFlights():
 	price = request.form['price']
 	status = request.form['status']
 	airplane_id = request.form['airplane_id']
+	
+	# check consistence of time
+	if departure_time >= arrival_time:
+		error = 'Error: wrong time format or inconsistent departure and arrival time!'
+		return render_template('airline_staff/update.html', result=error)
 
 	try:
+		msg = 'Create successfully!'
 		with conn.cursor() as cursor:
 			ins = 'INSERT INTO flight VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)'
 			cursor.execute(ins, (airline_name, flight_num, departure_airport, departure_time, 
 				arrival_airport, arrival_time, price, status, airplane_id))
 		conn.commit()
 	except MySQLError as e:
-		print('Got error {!r}, errno is {}'.format(e, e.args[0]))
+		msg = 'Got error {!r}, errno is {}'.format(e, e.args[0])
 	
-	return render_template('customer/index.html')
+	return render_template('airline_staff/update.html', result=msg)
 
 
 # Change status of flights
@@ -69,17 +77,19 @@ def changeFlightStatus():
 	flight_num = request.form['flight_num']
 	status = request.form['status']
 
-	# cursor used to send queries
-	cursor = conn.cursor()
-	# executes query
-	query = '''
-		UPDATE flight
-		SET status = %s
-		WHERE airline_name = %s AND flight_num = %s '''
-	cursor.execute(query, (status, airline_name, flight_num))
-	# stores the results in a variable
-	cursor.close()
-	return render_template('customer/index.html')
+	try:
+		msg = "Update successfully!"
+		with conn.cursor() as cursor:
+			query = '''
+				UPDATE flight
+				SET status = %s
+				WHERE airline_name = %s AND flight_num = %s '''
+			cursor.execute(query, (status, airline_name, flight_num))
+		conn.commit()
+	except MySQLError as e:
+		msg = 'Got error {!r}, errno is {}'.format(e, e.args[0])
+
+	return render_template('airline_staff/update.html', result=msg)
 
 
 # Add new airplane
@@ -92,14 +102,15 @@ def addNewAirplane():
 	seats = request.form['seats']
 
 	try:
+		msg = 'Add successfully!'
 		with conn.cursor() as cursor:
 			ins = 'INSERT INTO airplane VALUES(%s, %s, %s)'
 			cursor.execute(ins, (airline_name, airplane_id, seats))
 		conn.commit()
 	except MySQLError as e:
-		print('Got error {!r}, errno is {}'.format(e, e.args[0]))
+		msg = 'Got error {!r}, errno is {}'.format(e, e.args[0])
 	
-	return render_template('customer/index.html')
+	return render_template('airline_staff/update.html', result=msg)
 
 
 # Add new airport
@@ -108,17 +119,18 @@ def addNewAirplane():
 def addNewAirport():
 	# grabs information
 	airport_name = request.form['airport_name']
-	airport_city = request.form['airport_id']
+	airport_city = request.form['airport_city']
 
 	try:
+		msg = 'Add successfully!'
 		with conn.cursor() as cursor:
 			ins = 'INSERT INTO airport VALUES(%s, %s)'
 			cursor.execute(ins, (airport_name, airport_city))
 		conn.commit()
 	except MySQLError as e:
-		print('Got error {!r}, errno is {}'.format(e, e.args[0]))
+		msg = 'Got error {!r}, errno is {}'.format(e, e.args[0])
 	
-	return render_template('customer/index.html')
+	return render_template('airline_staff/update.html', result=msg)
 
 
 # View top5 booking agent
@@ -162,13 +174,21 @@ def viewTop5BookingAgent():
 	cursor.execute(query, (airline_name))
 	top5bycommission_past_year = cursor.fetchall()
 	cursor.close()
-	return render_template('airline_staff/index.html', 
+
+	# check data
+	msg = None
+	if top5bycount_past_year == None:
+		msg = 'No records in the last year!'
+	elif top5bycount_past_month == None:
+		msg = 'No records in the last month!'
+	return render_template('airline_staff/view.html', 
 		top5bycount_past_month=top5bycount_past_month,
 		top5bycount_past_year=top5bycount_past_year,
-		top5bycommission_past_year = top5bycommission_past_year)
+		top5bycommission_past_year=top5bycommission_past_year,
+		message_viewTop5BookingAgent=msg)
 
 
-# View Frequent Customers
+# View frequent customers
 @mod.route('/viewFrequentCustomers', methods=['POST'])
 @requires_login_airline_staff
 def viewFrequentCustomers():
@@ -186,8 +206,34 @@ def viewFrequentCustomers():
 		GROUP BY customer_email
 		ORDER by count DESC '''
 	cursor.execute(query, (airline_name))
-	result = cursor.fetchall()
-	return render_template('airline_staff/index.html', result=result)
+	data = cursor.fetchall()
+
+	if data:
+		return render_template('airline_staff/view.html', result_viewFrequentCustomers=data)
+	else:
+		msg = 'No records are found!'
+		return render_template('airline_staff/view.html', message_viewFrequentCustomers=msg)
+
+
+# View flights taken, sub module for view frequent customers
+@mod.route('/viewFlightsTaken', methods=['POST'])
+@requires_login_airline_staff
+def viewFlightsTaken():
+	# grabs information
+	airline_name = session['airline_name']
+	customer_email = request.form['customer_email']
+
+	# cursor used to send queries
+	cursor = conn.cursor()
+	# executes query
+	query = '''
+		SELECT customer_email, flight_num, purchase_date
+		FROM ticket NATURAL JOIN purchases
+		WHERE airline_name = %s AND customer_email = %s
+		ORDER by purchase_date DESC '''
+	cursor.execute(query, (airline_name, customer_email))
+	data = cursor.fetchall()
+	return render_template('airline_staff/view.html', result_viewFlightsTaken=data)	
 
 
 # View reports
@@ -196,22 +242,51 @@ def viewFrequentCustomers():
 def viewReports():
 	# grabs information
 	airline_name = session['airline_name']
-	start_date = request.form['start_date']
-	end_date = request.form['end_date']
+	start_month = request.form['start_month']
+	end_month = request.form['end_month']
+
+	# check consistence of months
+	if start_month > end_month:
+		error = 'Error: end month is earlier than start month!'
+		return render_template('airline_staff/view.html', message_viewReports=error)
+
+	# computes date
+	start_date = datetime.strptime(start_month, '%Y-%m').date()
+	start_date_str = start_date.strftime('%Y-%m-%d')
+	end_date = datetime.strptime(end_month, '%Y-%m').date() + relativedelta(months=+1)
+	end_date_str = end_date.strftime('%Y-%m-%d')
+	diff = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
 
 	# cursor used to send queries
 	cursor = conn.cursor()
-	# executes query
+	# query
 	query = '''
-		SELECT YEAR(purchase_time) as year, MONTH(purchase_time) as month, COUNT(ticket_id) as total
+		SELECT COUNT(ticket_id) as total
 		FROM purchases NATURAL JOIN ticket
-		WHERE airline_name = %s AND 
-			purchase_date BETWEEN %s AND %s
-		GROUP BY year, month 
-		ORDER BY year DESC, month DESC '''
-	cursor.execute(query, (airline_name, start_date, end_date))
-	result = cursor.fetchall()
-	return render_template('airline_staff/index.html', result=result)
+		WHERE airline_name = %s AND purchase_date >= %s AND purchase_date < %s '''
+	
+	# total
+	cursor.execute(query, (airline_name, start_date_str, end_date_str))
+	data = cursor.fetchone()
+	total = data['total']
+
+	# monthwise
+	monthwise_label = []
+	monthwise_total = []
+	end_date = start_date + relativedelta(months=+1)
+	for _ in range(diff):
+		start_date_str = start_date.strftime('%Y-%m-%d')
+		end_date_str = end_date.strftime('%Y-%m-%d')
+		cursor.execute(query, (airline_name, start_date_str, end_date_str))
+		monthwise = cursor.fetchone()
+		monthwise_label.append(start_date.strftime('%y/%m'))
+		monthwise_total.append(monthwise['total'] if monthwise['total'] != None else 0)
+		start_date += relativedelta(months=+1)
+		end_date += relativedelta(months=+1)
+
+	cursor.close()
+	return render_template('airline_staff/view.html', total=total, 
+		monthwise_label=monthwise_label, monthwise_total=monthwise_total)
 
 
 # Compare revenue
@@ -291,9 +366,17 @@ def viewTop3Destinations():
 	cursor.execute(query, (airline_name))
 	top3_past1year = cursor.fetchall()
 	cursor.close()
-	return render_template('airline_staff/index.html', 
+
+	# check data
+	msg = None
+	if top3_past1year == None:
+		msg = 'No records in the last year!'
+	elif top3_past3month == None:
+		msg = 'No records in the last 3 months!'
+	return render_template('airline_staff/view.html', 
 		top3_past3month=top3_past3month,
-		top3_past1year=top3_past1year)
+		top3_past1year=top3_past1year,
+		message_viewTop3Destinations=msg)
 
 
 # Define route for logout
